65604ad3152a6eef8218716908b0401d6ed09f10
anxiuze <anxiuze@163.com>
2020-08-25 23:50:22 +0800

    Thread.join();

--- UsualTest/src/test/java/test0820/ThreadYieldTest.java
+++ UsualTest/src/test/java/test0820/ThreadYieldTest.java
@@ -1,5 +1,7 @@
 package test0820;
 
+import org.junit.Test;
+
 public class ThreadYieldTest {
     public static void main(String[] args) {
         MyThreadYield myThreadYield = new MyThreadYield();
@@ -8,11 +10,32 @@ public class ThreadYieldTest {
         thread1.start();
         thread2.start();
     }
+
+    /**
+     * join()的作用是：“等待该线程终止”
+     * */
+    @Test
+    public void testJoin() {
+        MyJoin myJoin = new MyJoin();
+
+        Thread thread = new Thread(myJoin);
+        thread.start();
+        for (int i = 0; i < 100; i++) {
+            if (i == 50) {
+                try {
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(Thread.currentThread().getName() + i);
+        }
+    }
 }
 
 /**
  * Thread.yield() 方法，使当前线程由执行状态，变成为就绪状态，让出cpu时间，在下一个线程执行时候，此线程有可能被执行，也有可能没有被执行
- * */
+ */
 class MyThreadYield implements Runnable {
 
     @Override
@@ -22,3 +45,13 @@ class MyThreadYield implements Runnable {
         System.out.println(Thread.currentThread().getName() + "执行结束！");
     }
 }
+
+class MyJoin implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 200; i++) {
+            System.out.println("线程VIP" + i);
+        }
+    }
+}
+
