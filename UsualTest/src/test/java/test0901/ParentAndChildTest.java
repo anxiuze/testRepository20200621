69b7aba99492e1857ac6c7aee7df2de8e3af4a86
anxiuze <anxiuze@163.com>
2020-09-16 22:37:45 +0800

    父子类构造方法、静态方法、实例方法执行过程

---
+++ UsualTest/src/test/java/test0901/ParentAndChildTest.java
@@ -0,0 +1,45 @@
+package test0901;
+
+import java.util.Calendar;
+
+/**
+ * @author anxiuze
+ * @date 2020/9/16 22:28
+ * @Description:
+ */
+public class ParentAndChildTest {
+
+    public static void main(String[] args) {
+        ChildrenClass childrenClass = new ChildrenClass();
+        childrenClass.method();
+    }
+}
+
+class ParentClass {
+    public ParentClass() {
+        System.out.println("A");
+    }
+
+    public static void staticMethod() {
+        System.out.println("B");
+    }
+
+    public void method() {
+        System.out.println("C");
+    }
+}
+
+class ChildrenClass extends ParentClass {
+    public ChildrenClass() {
+        System.out.println("X");
+    }
+
+    public static void staticMethod() {
+        System.out.println("Y");
+    }
+
+    public void method() {
+        System.out.println("Z");
+    }
+
+}
